f39055dca6e96ece9b76aa982cfd3ff72a1a3879
iliyas <iliyassipai861@gmail.com>
2025-08-14 18:58:10 +0530

    update signup component

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -2,25 +2,9 @@ import React, { useState } from "react";
 import "./signup.css";
 
 function Signup() {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    password: "",
-  });
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Form Data:", formData);
-    alert("Signup Successful!");
-  };
-
   return (
     <div className="signup-container">
-      <form className="signup-form" onSubmit={handleSubmit}>
+      <form className="signup-form" >
         <h2>Create Account</h2>
 
         <label htmlFor="name">Full Name</label>
@@ -29,8 +13,6 @@ function Signup() {
           id="name"
           name="name"
           placeholder="Enter your full name"
-          value={formData.name}
-          onChange={handleChange}
           required
         />
 
@@ -40,8 +22,6 @@ function Signup() {
           id="email"
           name="email"
           placeholder="Enter your email"
-          value={formData.email}
-          onChange={handleChange}
           required
         />
 
@@ -51,8 +31,6 @@ function Signup() {
           id="password"
           name="password"
           placeholder="Enter your password"
-          value={formData.password}
-          onChange={handleChange}
           required
         />
 
